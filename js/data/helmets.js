8f24a753fa9e774793c8d45a6247de349ebaeaa2
Maria McConkey <maliya241@users.noreply.github.com>
2023-01-08 08:42:59 -0500

    Create helmets.js

---
+++ js/data/helmets.js
@@ -0,0 +1,28 @@
+/**************	
+To add more items, copy following code and enter relevant information in between ''.
+
+items_array[items_array.length] = {
+	item_category:'',
+	item_title:'',
+	href:'', 
+	picture_href:'',
+	tags:''
+};
+
+**************/
+
+items_array[items_array.length] = {
+	item_category:'helmets',
+	item_title:'OPS-CORE FAST XP LEGACY HIGH CUT HELMET',
+	href:'https://shop.gentexcorp.com/ops-core-fast-xp-legacy-high-cut-helmet/', 
+	picture_href:'https://cdn11.bigcommerce.com/s-tdvizl1wrj/images/stencil/500x659/products/846/7797/7797__43837.1671646799.jpg?c=2',
+	tags:'helmet professional use'
+};
+
+items_array[items_array.length] = {
+	item_category:'helmets',
+	item_title:'EXFIL® BALLISTIC',
+	href:'https://www.teamwendy.com/products/helmets-accessories/helmets/exfil-ballistic', 
+	picture_href:'https://www.teamwendy.com/TeamWendy/media/TeamWendyMediaLibrary/Products/Helmets/EXFIL%20Ballistic/BALLISTIC_RAIL_3_COYOTE_BROWN_ANGLE.jpg?height=340&width=470',
+	tags:'helmet enthusiast'
+};
