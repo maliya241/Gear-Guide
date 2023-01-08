671f441e2031edecd2714e8c953019813cfa7cdf
Maria McConkey <maliya241@users.noreply.github.com>
2023-01-08 08:43:14 -0500

    Add files via upload

---
+++ js/data/monocular_night_vision.js
@@ -0,0 +1,21 @@
+/**************	
+To add more items, copy following code and enter relevant information in between ''.
+
+items_array[items_array.length] = {
+	item_category:'',
+	item_title:'',
+	href:'', 
+	picture_href:'',
+	tags:''
+};
+
+**************/
+	
+items_array[items_array.length] = {
+	item_category:'monocular_night_vision',
+	item_title:'AN/PVS-14A GEN III Green Phosphor "ZS" Night Vision Monocular',
+	href:'https://usnightvision.com/an-pvs-14a-gen-iii-green-phosphor-zs-night-vision-monocular/', 
+	picture_href:'https://cdn11.bigcommerce.com/s-6d8s7h/images/stencil/200x60/products/1463/8209/14newmain1__46765.1642082405.jpg?c=2',
+	tags:'helmet accessory nightvision night vision monocular gp professional use'
+};
+
